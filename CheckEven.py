c6dbd5c39bfb730da0f4354bdfd4913b9512b88d
RuiPengGao <35279563+RuiPengGao@users.noreply.github.com>
2018-01-31 12:58:47 +0800

    Delete CheckEven.py

--- CheckEven.py
+++
@@ -1,8 +0,0 @@
-#to check if an integer is even or odd
-
-num = int(input("Enter an integer: "))
-if (num % 2) == 0:
-    print("{0} is even".format(num))
-else:
-    print("{0} is odd".format(num))
-          
